e908681a8826b8af343f2767741090759f6079a1
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-31 09:43:04 +0530

    Add files via upload

---
+++ 29.distance_formula.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   double x1=3,y1=3;
+   double x2=4,y2=4;
+   double ans=sqrt(pow(x2-x1,2)*1.0 + pow(y2-y1,2)*1.0);
+   cout<<ans;
+}
